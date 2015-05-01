a98421f95317dae5b621a7774df86baeabd4fb93
Nate Finch <nate.finch@gmail.com>
2015-05-01 16:41:19 -0400

    show how to use stderr for logging

--- example_plugin/main.go
+++ example_plugin/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/natefinch/plugin"
 )
 
 func main() {
+	log.SetPrefix("[plugin log] ")
+
 	plugin.Provide("Plugin", api{})
 }
 
 type api struct{}
 
 func (api) SayHi(name string, response *string) error {
+	log.Printf("got call for SayHi with name %q", name)
+
 	*response = "Hi " + name
 	return nil
 }
