a6d83524c07cfa4bce1c6df41893f848429d4dd0
Nate Finch <nate.finch@gmail.com>
2015-05-24 22:39:42 -0400

    one more example

--- example_test.go
+++ example_test.go
@@ -13,10 +13,10 @@ import (
 // This function should be called from the master program that wants to run
 // plugins to extend its functionality.
 //
-// StartProviderCodec starts a plugin at path "/var/lib/foo", using the JSON-RPC
-// codec, and writing its output to this application's Stderr.  The application
-// can then call methods on the rpc client returned using the standard rpc
-// pattern.
+// This example shows the master program starting a plugin at path
+// "/var/lib/foo", using JSON-RPC, and writing its output to this application's
+// Stderr.  The application can then call methods on the rpc client returned
+// using the standard rpc pattern.
 func ExampleStartProviderCodec() {
 	client, err := plugin.StartProviderCodec(jsonrpc.NewClientCodec, os.Stderr, "/var/lib/foo")
 	if err != nil {
@@ -29,16 +29,26 @@ func ExampleStartProviderCodec() {
 // This function should be called from the plugin program that wants to provide
 // functionality for the master program.
 //
-// NewProviderCodec starts an RPC server that reads from stdin and writes to
-// stdout. It provides functions attached to the API value passed in.
-// Server.Serve() will block forever, so it is common to simply put this at the
-// end of the plugin's main function.
+// This example shows the plugin starting a JSON-RPC server to be accessed by
+// the master program. Server.ServeCodec() will block forever, so it is common
+// to simply put this at the end of the plugin's main function.
 func ExampleProvider_ServeCodec() {
 	p := plugin.NewProvider()
 	if err := p.RegisterName("Foo", API{}); err != nil {
 		log.Fatalf("can't register api: %s", err)
 	}
 	p.ServeCodec(jsonrpc.NewServerCodec)
+}
+
+// This function should be called from the plugin program that wants to consume
+// an API from the master program.
+//
+// This example shows the plugin creating a JSON-RPC client talks to the host
+// application.
+func ExampleNewConsumerCodec() {
+	client := plugin.NewConsumerCodec(jsonrpc.NewServerCodec)
+	var reply string
+	client.Call("Foo.ToUpper", "something", &reply)
 }
 
 // API is an example type to show how to serve methods over RPC.
